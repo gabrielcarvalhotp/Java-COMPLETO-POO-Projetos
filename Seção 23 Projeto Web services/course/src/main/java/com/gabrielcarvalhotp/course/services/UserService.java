113274ab39a744574a4db0f871fe954322494b44
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2024-04-06 13:57:56 -0300

    Project API Restful + MongoDB

--- Seção 23 Projeto Web services/course/src/main/java/com/gabrielcarvalhotp/course/services/UserService.java
+++ Seção 23 Projeto Web services/course/src/main/java/com/gabrielcarvalhotp/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.gabrielcarvalhotp.course.entities.User;
 import com.gabrielcarvalhotp.course.repositories.UserRepository;
 import com.gabrielcarvalhotp.course.services.exceptions.DatabaseException;
 import com.gabrielcarvalhotp.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update( Integer id, User obj) {
-        User user = userRepository.getReferenceById(id);
-        updateData(obj, user);
-        return userRepository.save(user);
+        try {
+            User user = userRepository.getReferenceById(id);
+            updateData(obj, user);
+            return userRepository.save(user);
+        } catch (EntityNotFoundException exception) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private static void updateData(User source, User target) {
